143481fb47f7fabeb1761e1f6554f1ce5324b07c
renkobayashi1234 <ren_kobayashi@jbat.co.jp>
2024-05-07 11:06:25 +0900

    add post function

--- src/main/java/com/example/smacsocket/service/AccessLogService.java
+++ src/main/java/com/example/smacsocket/service/AccessLogService.java
@@ -2,6 +2,7 @@ package com.example.smacsocket.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,11 @@ import org.springframework.web.client.RestTemplate;
 public class AccessLogService {
     private Logger logger = LoggerFactory.getLogger(AccessLogService.class);
 
+    @Value("${alert-server.address}")
+    private String alertServerAddress;
+
     public String sendPostRequest(String alertNo){
-        String url="http://localhost:8000/post?alertNo="+alertNo;
+        String url=alertServerAddress+"/post?alertNo="+alertNo;
 
         RestTemplate restTemplate=new RestTemplate();
 
